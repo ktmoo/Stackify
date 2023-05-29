54394a23263a4590e0a1e595b7d86e69651ffeef
B C SAMRUDH <114090255+bcsamrudh@users.noreply.github.com>
2023-05-29 20:01:00 +0530

    Update urls.py

--- Stackify/post/urls.py
+++ Stackify/post/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('post/create_a_post', views.create_a_post,name="create_a_post" ),
@@ -9,3 +11,8 @@ urlpatterns = [
     path('post/upvote/<str:slug>', views.upvote,name="upvote" ),
     path('post/search',views.search_view,name="search")
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
